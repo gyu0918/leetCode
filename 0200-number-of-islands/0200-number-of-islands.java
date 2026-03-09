e3fd24cbc9e4a79e24e34a92e59efd08f59299a0
김정규 <111268088+gyu0918@users.noreply.github.com>
2026-03-09 16:53:24 +0900

    Time: 5 ms (30.24%), Space: 51.7 MB (80.67%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -19,8 +19,9 @@
 
 class Solution {
     static boolean[][] visited;
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,-1,1};
+    static int[] drow = {-1,1,0,0};
+    static int[] dcol = {0,0,-1,1};
+
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
@@ -30,36 +31,35 @@ class Solution {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1' && !visited[i][j]) {
-                    bfs(grid, visited, i, j);
+                    bfs(grid,  i, j);
                     count++;
                 }
             }
         }
-
         return count;
     }
 
-    private void bfs(char[][] grid, boolean[][] visited, int x, int y) {
+    private void bfs(char[][] grid, int row, int col) {
         int m = grid.length;
         int n = grid[0].length;
 
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{x, y});
-        visited[x][y] = true;
+        Deque<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{row, col});
+        visited[row][col] = true;
 
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
-            int cx = cur[0];
-            int cy = cur[1];
+            int crow = cur[0];
+            int ccol = cur[1];
 
             for (int i = 0; i < 4; i++) {
-                int nx = cx + dx[i];
-                int ny = cy + dy[i];
+                int nrow = crow + drow[i];
+                int ncol = ccol + dcol[i];
 
-                if (nx >= 0 && nx < m && ny >= 0 && ny < n) {
-                    if (grid[nx][ny] == '1' && !visited[nx][ny]) {
-                        visited[nx][ny] = true;
-                        queue.offer(new int[]{nx, ny});
+                if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n) {
+                    if (grid[nrow][ncol] == '1' && !visited[nrow][ncol]) {
+                        visited[nrow][ncol] = true;
+                        queue.offer(new int[]{nrow, ncol});
                     }
                 }
             }
