a7c0efc779da4081822054fe9c21775646e5e1b8
김정규 <111268088+gyu0918@users.noreply.github.com>
2026-03-30 17:12:09 +0900

    Time: 2 ms (90.25%), Space: 44.2 MB (48.04%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -3,58 +3,54 @@ import java.io.*;
 
 class Solution {
     static int[][] grid;
-    static boolean[][] check;
     static int[] dx = {1,-1,0,0};
-    static int[] dy = {0,0,1,-1}; 
+    static int[] dy = {0,0,1,-1};
     
     public static int orangesRotting(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-
-        Queue<int[]> queue = new LinkedList<>();
+        
+        Queue<int[]> q = new LinkedList<>();
         int freshCount = 0;
 
-        // 처음 썩은 오렌지 전부 큐에 넣기
-        // fresh orange 개수 세기
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 2) {
-                    queue.offer(new int[]{i, j});
-                } else if (grid[i][j] == 1) {
+        //썩은 오렌지 처음부터 전부 큐에 넣기 
+        // frsh orange 개수 세기 
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if (grid[i][j] == 2){
+                    q.offer(new int[]{i,j});
+                } else if (grid[i][j] == 1){
                     freshCount++;
                 }
             }
         }
 
-        // 처음부터 fresh orange가 없으면 0
-        if (freshCount == 0) {
+        //예외 처리 처음부터 frsh orange가 없으면 0으로 처리 
+        if (freshCount == 0){
             return 0;
         }
-
         int minutes = 0;
-
-        while (!queue.isEmpty()) {
-            int size = queue.size();
+        
+        while (!q.isEmpty()){
+            int size = q.size();
             boolean rottenThisRound = false;
-
-            for (int i = 0; i < size; i++) {
-                int[] current = queue.poll();
-                int x = current[0];
-                int y = current[1];
-
-                for (int d = 0; d < 4; d++) {
+            
+            for (int i = 0; i < size; i++){
+                int[] cur = q.poll();
+                int x = cur[0];
+                int y = cur[1];
+                for (int d = 0; d < 4; d++){
                     int nx = x + dx[d];
                     int ny = y + dy[d];
 
-                    if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == 1) {
-                        grid[nx][ny] = 2;   // fresh -> rotten
+                    if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == 1){
+                        grid[nx][ny] = 2;
                         freshCount--;
-                        queue.offer(new int[]{nx, ny});
+                        q.offer(new int[]{nx,ny});
                         rottenThisRound = true;
                     }
                 }
             }
-
             // 이번 레벨에서 실제로 썩은 오렌지가 생겼을 때만 1분 증가
             if (rottenThisRound) {
                 minutes++;
@@ -73,7 +69,7 @@ class Solution {
         int n = Integer.parseInt(st.nextToken());
 
         grid = new int[m][n];
-        check = new boolean[m][n];
+
         //표 생성
         for (int i = 0; i < m; i++){
             st = new StringTokenizer(br.readLine());
