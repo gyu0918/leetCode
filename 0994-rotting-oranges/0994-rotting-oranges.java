68a2942a551c61c67fbf7d75453deeef75ea9212
김정규 <111268088+gyu0918@users.noreply.github.com>
2026-04-01 22:25:27 +0900

    Time: 2 ms (90.13%), Space: 44.5 MB (19.81%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,7 +1,94 @@
+// import java.util.*;
+// import java.io.*;
+
+// class Solution {
+//     static int[][] grid;
+//     static int[] dx = {1,-1,0,0};
+//     static int[] dy = {0,0,1,-1};
+    
+//     public static int orangesRotting(int[][] grid) {
+//         int m = grid.length;
+//         int n = grid[0].length;
+        
+//         Queue<int[]> q = new LinkedList<>();
+//         int freshCount = 0;
+
+//         //썩은 오렌지 처음부터 전부 큐에 넣기 
+//         // frsh orange 개수 세기 
+//         for (int i = 0; i < m; i++){
+//             for (int j = 0; j < n; j++){
+//                 if (grid[i][j] == 2){
+//                     q.offer(new int[]{i,j});
+//                 } else if (grid[i][j] == 1){
+//                     freshCount++;
+//                 }
+//             }
+//         }
+
+//         //예외 처리 처음부터 frsh orange가 없으면 0으로 처리 
+//         if (freshCount == 0){
+//             return 0;
+//         }
+//         int minutes = 0;
+        
+//         while (!q.isEmpty()){
+//             int size = q.size();
+//             boolean rottenThisRound = false;
+            
+//             for (int i = 0; i < size; i++){
+//                 int[] cur = q.poll();
+//                 int x = cur[0];
+//                 int y = cur[1];
+//                 for (int d = 0; d < 4; d++){
+//                     int nx = x + dx[d];
+//                     int ny = y + dy[d];
+
+//                     if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == 1){
+//                         grid[nx][ny] = 2;
+//                         freshCount--;
+//                         q.offer(new int[]{nx,ny});
+//                         rottenThisRound = true;
+//                     }
+//                 }
+//             }
+//             // 이번 레벨에서 실제로 썩은 오렌지가 생겼을 때만 1분 증가
+//             if (rottenThisRound) {
+//                 minutes++;
+//             }
+//         }
+
+//         // 아직 fresh orange가 남아있으면 불가능
+//         return freshCount == 0 ? minutes : -1;
+//     }
+
+//     public static void main(String[] args) throws IOException{
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+//         StringTokenizer st = new StringTokenizer(br.readLine());
+//         int m = Integer.parseInt(st.nextToken());
+//         int n = Integer.parseInt(st.nextToken());
+
+//         grid = new int[m][n];
+
+//         //표 생성
+//         for (int i = 0; i < m; i++){
+//             st = new StringTokenizer(br.readLine());
+//             for (int j = 0; j < n; j++){
+//                 grid[i][j]= Integer.parseInt(st.nextToken());
+//             }
+//         }
+
+//         System.out.println(orangesRotting(grid));
+
+//     }
+
+// }
+
 import java.util.*;
 import java.io.*;
 
 class Solution {
+    
     static int[][] grid;
     static int[] dx = {1,-1,0,0};
     static int[] dy = {0,0,1,-1};
@@ -9,72 +96,74 @@ class Solution {
     public static int orangesRotting(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        
-        Queue<int[]> q = new LinkedList<>();
-        int freshCount = 0;
 
-        //썩은 오렌지 처음부터 전부 큐에 넣기 
-        // frsh orange 개수 세기 
+        Queue<int[]> q = new LinkedList<>();
+
+        int countOrange = 0;
+        //find a rotten apple and stretch it out from there
         for (int i = 0; i < m; i++){
             for (int j = 0; j < n; j++){
-                if (grid[i][j] == 2){
+                if(grid[i][j] == 2){
                     q.offer(new int[]{i,j});
-                } else if (grid[i][j] == 1){
-                    freshCount++;
+                }else if (grid[i][j] == 1){
+                    countOrange++;
                 }
             }
         }
 
-        //예외 처리 처음부터 frsh orange가 없으면 0으로 처리 
-        if (freshCount == 0){
+        //exceptional handling 
+        if (countOrange == 0){
             return 0;
         }
-        int minutes = 0;
-        
-        while (!q.isEmpty()){
+
+        int criteria = 0;
+        // use queue and start bfs
+        while(!q.isEmpty()){
             int size = q.size();
             boolean rottenThisRound = false;
-            
             for (int i = 0; i < size; i++){
                 int[] cur = q.poll();
                 int x = cur[0];
                 int y = cur[1];
-                for (int d = 0; d < 4; d++){
-                    int nx = x + dx[d];
-                    int ny = y + dy[d];
-
+                for (int j = 0; j < 4; j++){
+                    int nx = x + dx[j];
+                    int ny = y + dy[j];
                     if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == 1){
                         grid[nx][ny] = 2;
-                        freshCount--;
+                        countOrange--;
                         q.offer(new int[]{nx,ny});
                         rottenThisRound = true;
                     }
                 }
-            }
-            // 이번 레벨에서 실제로 썩은 오렌지가 생겼을 때만 1분 증가
-            if (rottenThisRound) {
-                minutes++;
-            }
-        }
 
+
+                
+                }
+                // 이번 레벨에서 실제로 썩은 오렌지가 생겼을 때만 1분 증가
+                if (rottenThisRound) {
+                    criteria++;
+            }
+            
+        }
         // 아직 fresh orange가 남아있으면 불가능
-        return freshCount == 0 ? minutes : -1;
+        return countOrange == 0 ? criteria : -1;
+
+
     }
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int m = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
 
         grid = new int[m][n];
-
-        //표 생성
+        //표 그리기
         for (int i = 0; i < m; i++){
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++){
-                grid[i][j]= Integer.parseInt(st.nextToken());
+                grid[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
